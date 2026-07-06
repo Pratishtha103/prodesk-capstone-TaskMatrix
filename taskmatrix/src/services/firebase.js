da69547e440503b050756578ace42d776ecbb454
Pratishtha103 <11f23pratishtha@gmail.com>
2026-07-06 23:31:14 +0530

    vercel deployment issue fix

--- taskmatrix/src/services/firebase.js
+++ taskmatrix/src/services/firebase.js
@@ -9,6 +9,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.log("API KEY EXISTS:", !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("API KEY VALUE:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("AUTH DOMAIN:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
